edd396b966872b37645b59ee2ceb1b26248acd08
Helio Sales Jr <heliosalesjr@gmail.com>
2024-06-12 10:33:57 -0400

    building the app layout

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import CartOverview from '../features/cart/CartOverview'
+import Header from './Header'
+
+function AppLayout() {
+  return (
+    <>
+        <Header />
+        <main>
+            
+            <Outlet />
+        </main>
+        
+        <CartOverview />
+    </>
+  )
+}
+
+export default AppLayout
---
+++ src/ui/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Header() {
+  return (
+    <header>
+        <Link>
+            Helio's Pizzas
+        </Link>
+        <p>Helio</p>
+    </header>
+  )
+}
+
+export default Header
